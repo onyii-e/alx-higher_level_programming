28bb79570966490e6040866482a7670be7b69c45
onyii-e <onnyinyechinwaneri00@gmail.com>
2023-11-27 14:53:22 -0800

    size_t print_listint(const listint_t *h); listint_t *add_nodeint(listint_t **head, const int n); void free_listint(listint_t *head); int check_cycle(listint_t *list);

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H
+#define _LISTS_H
 
 #include <stdlib.h>
 
@@ -14,4 +14,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif
+
