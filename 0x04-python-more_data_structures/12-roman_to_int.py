b328b1acd008b651781394be52b5fedd226b63a9
onyii-e <onnyinyechinwaneri00@gmail.com>
2023-12-06 05:48:57 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
 
     roman_numerals = {
             'I': 1, 'X': 10, 'L': 50, 'V': 5,
-            'C': 100, 'D' : 500, 'M': 1000
+            'C': 100, 'D': 500, 'M': 1000
             }
 
     result = 0
