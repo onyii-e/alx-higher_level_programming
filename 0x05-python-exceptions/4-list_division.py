d30162430e5da02c9101d86e86f6a2cfed2a533b
onyii-e <onyinyechinwaneri00@gmail.com>
2023-12-18 16:27:09 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
     Returns:
         A new list of length list_length containing all the divisions
     """
-    new_list  []
+    new_list = []
     for i in range(0, list_length):
         try:
             div  my_list_1[i] / my_list_2[i]
@@ -27,4 +27,3 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(div)
         return (new_list)
-
