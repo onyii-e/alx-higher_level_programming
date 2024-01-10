7d663be7dc3c5eaea9689ef36aa49bf9fa172af2
onyii-e <onyinyechinwaneri00@gmail.com>
2024-01-09 17:49:46 -0800

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,7 +10,7 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
-    text= ""
+    text = ""
     with open(filename) as r:
         for line in r:
             text += line
