6ac2241390e3ade9b64835ff2c8fa271e326abcb
onyii-e <onyinyechinwaneri00@gmail.com>
2024-01-09 17:32:43 -0800

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,7 +5,7 @@
 class Student:
     """Represent a student."""
 
-    def __init(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
 
         Args:
