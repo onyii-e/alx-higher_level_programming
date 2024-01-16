00f63d4aa4d1f5bf1f48934d405f7deb14335b87
onyii-e <onyinyechinwaneri00@gmail.com>
2024-01-15 16:31:26 -0800

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-        @staticmethod
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''Jsonifies a dictionary so it's quite rightly and longer.'''
         if list_dictionaries is None or not list_dictionaries:
