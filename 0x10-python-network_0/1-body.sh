3308d0512a0ec57bb71f06e827c4e96cb5869c05
onyii-e <onyinyechinwaneri00@gmail.com>
2024-07-11 16:33:25 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if the user provided a URL argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send GET request to the provided URL and display the body if status code is 200
-curl -s -o /tmp/body -w "%{http_code}" "$1" > /dev/null
-status_code=$(cat /tmp/body)
-if [ "$status_code" -eq 200 ]; then
-    curl -s "$1"
-fi
+# This script sends a GET request to a URL and displays the body of the response (if status code is 200)
+curl -sL "$1"
