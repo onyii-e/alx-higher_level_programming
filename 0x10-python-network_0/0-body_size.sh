33f93c2fe0b77cdc80751c40fd31c34d778438db
onyii-e <onyinyechinwaneri00@gmail.com>
2024-07-11 16:31:04 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if the user provided a URL argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to the provided URL and measure the body size
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# This script sends a request to a URL and displays the size of the response body in bytes
+curl -s "$1" | wc -c
